ec667eb7caf91f61af990136d79a3463808970d0
Phil Fernandez <philfernandez@protonmail.com>
2023-11-25 17:27:20 -0800

    padding

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,54 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Game, GameObjects, Scene, Types, WEBGL } from 'phaser';
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector('canvas#game') as HTMLCanvasElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+class GameScene extends Scene {
+    private textbox: GameObjects.Text | undefined;
+
+    constructor() {
+        super('scene-game');
+    }
+
+    create() {
+        this.textbox = this.add.text(
+            window.innerWidth / 2,
+            window.innerHeight / 2,
+            'Welcome to Phaser X Vite!',
+            {
+                color: '#FFF',
+                fontFamily: 'monospace',
+                fontSize: '26px'
+            }
+        );
+
+        this.textbox.setOrigin(0.5, 0.5);
+    }
+
+    update(time: number, delta: number) {
+        if (!this.textbox) {
+            return;
+        }
+
+        this.textbox.rotation += 0.0005 * delta;
+    }
+}
+
+const config = {
+    type: WEBGL,
+    width: document.querySelector('div.container')?.clientWidth,
+    height: document.querySelector('div.container')?.clientHeight,
+    canvas,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            // debug: true
+        }
+    },
+    scene: [
+        GameScene
+    ]
+};
+
+new Game(config);
